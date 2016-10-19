f09bea64b12af39f77c2634859104e72d2526a50
Diego Figueroa <diego.fv94@gmail.com>
2016-10-18 20:11:22 -0400

    add step2 API call

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var esrever = require('esrever');
 var unirest = require('unirest');
 var token = '8608e5d953f1b41486e51bed97e58fa8';
 
@@ -20,6 +21,29 @@ router.get('/step1', function(req, res) {
     .end(function(response){
       console.log('Final response: '+ response);
       res.send(response);
+    });
+});
+
+router.get('/step2', function(req, res) {
+  unirest.post('http://challenge.code2040.org/api/reverse')
+    .type('json')
+    .send({
+      token: token,
+    })
+    .end(function(string){
+      console.log('Step 2 string: '+ string.body);
+      var reverse = esrever.reverse(string.body);
+      console.log('Step 2 reversed: '+ reverse);
+      unirest.post('http://challenge.code2040.org/api/reverse/validate')
+        .type('json')
+        .send({
+          token: token,
+          string: reverse,
+        })
+        .end(function(response){
+          console.log('Final response: '+ response);
+          res.send(response);
+        });
     });
 });
 
